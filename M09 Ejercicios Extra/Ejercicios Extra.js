46cc034bf7bfd61b9ac5be2e352af857ffc696c9
JulioSesai <antonio.sesai@gmail.com>
2023-07-19 22:52:42 -0600

    Test 09 finalizado se me hizo dificil

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,8 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   return Object.entries(objeto);
+
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +16,25 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   
+const nuevObjeto = {};
+
+for (let i = 0; i < string.length; i++) {
+    const element = string[i];
+    if(nuevObjeto[element]){
+        nuevObjeto[element]++;
+    }else{
+        nuevObjeto[element] = 1;
+    }
+}
+
+const clavesOrdenadas = Object.keys(nuevObjeto).sort(); //object keys construye un array al pasarle un objeto objeto en array ordena alfabeticamente el metodo sort
+const objetoOrdenado = {};
+for (let i = 0; i < clavesOrdenadas.length; i++) {
+    const clave = clavesOrdenadas[i];
+    objetoOrdenado[clave] = nuevObjeto[clave];
+}
+   return objetoOrdenado;
 }
 
 function capToFront(string) {
@@ -22,6 +43,12 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   const palabraArray = string.split(''); //array de letras del string 
+   const aMayusculas = palabraArray.filter( letra => letra === letra.toUpperCase()); //filtra mayus 
+   const aMinusculas = palabraArray.filter(letra => letra === letra.toLowerCase()); //filtra minus
+   const uneMayusMinus = aMayusculas.concat(aMinusculas).join(''); //Concatena mayus con minus y las une en un solo string
+   return uneMayusMinus; 
+
 }
 
 function asAmirror(frase) {
@@ -29,18 +56,36 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   const palabraArray = frase.split(' '); //array de letras del string 
+   const palabrasInvertidas = palabraArray.map(palabra => palabra.split('').reverse().join(''));
+   const fraseInvertida = palabrasInvertidas.join(' ');
+   return fraseInvertida;
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+
+   const stringNumero = String(numero); //convertir numero en string
+   const stringInvertido = stringNumero.split('').reverse().join(''); //invertir el sentido
+   
+   if (stringNumero === stringInvertido) { //evaluar si son iguales de manera normal e invertido
+      return 'Es capicua';
+   } else {
+      return 'No es capicua';
+   }
+       
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   const expReg = /[abc]/gi;
+   const nuevoString = string.replace(expReg, '');
+   return nuevoString;
+
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +94,9 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   //El metodo sort es un metodo de ordenamiento que evalua a y b 
+   const arrayOrdenadoPorLongitud = arrayOfStrings.sort((a, b) => a.length - b.length);
+   return arrayOrdenadoPorLongitud;
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +106,8 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   const interseccion = array1.filter((element) => array2.includes(element));
+   return interseccion;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
